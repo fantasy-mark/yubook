80ae3c4437772f1b6d7496c0d458571d8ca354c1
mark <hacker.do@163.com>
2018-11-22 16:09:59 +0800

    自实现红图代替BluePrint(用于模块),使之更合理

---
+++ app/api/v1/book.py
@@ -0,0 +1,7 @@
+from app.libs.redprint import RedPrint
+
+api = RedPrint('book')
+
+@api.route('/get')
+def get_book():
+    return 'a book'
---
+++ app/api/v1/user.py
@@ -0,0 +1,7 @@
+from app.libs.redprint import RedPrint
+
+api = RedPrint('user')
+
+@api.route('/get')
+def get_user():
+    return 'a user'
